bf507e149d08ac309bad730ca7edf7b0e558b65b
Manisha <62801320+maniishaww@users.noreply.github.com>
2024-07-26 22:43:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+import cv2
+from PIL import Image
+
+from sklearn.linear_model import LogisticRegression
+import pickle
+
+
+
+# Loading the pre-trained model
+model = pickle.load(open(r"C:\Users\Manisha\DATA SCIENCE\MACHINE LEARNING Phase 1\Dog breeds\dataset\Yorkshire_Terrier\log.pkl","rb"))
+
+
+# Title of the page
+    
+st.title("Dogs Breed Prediction")
+
+#User input
+image = st.file_uploader("Upload an image", type = ['jpg','jpeg','png'])
+
+# Check if the mail is empty or not
+if st.button('Submit'):
+    if image:
+        image = Image.open(image)
+        image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+        res = cv2.resize(image,(150,150))
+        flat = res.flatten()
+        model.predict([flat])[0]
+        st.image(image, caption='Uploaded Image', width=200)
+#             st.image(r"C:\Users\Manisha\Downloads\spam image.png")
